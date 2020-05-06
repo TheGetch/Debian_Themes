1d7b241184acfbd9a93651e2f856c533b05e660d
TheGetch <TheGetch@users.noreply.github.com>
2020-05-06 15:33:42 -0400

    automation

---
+++ debian_config.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root or sudo"
+  exit
+fi
+
+echo '########################### apt update'
+apt update
+
+echo '########################### Installing gtk2-egines-murrine'
+apt install gtk2-egines-murrine
+
+echo '########################### Installing lightdm-gtk-greeter-settings'
+apt install lightdm-gtk-greeter-settings
+
+echo '########################### Installing Material-Black-Plum_1.8.1'
+cd /usr/share/themes/
+wget https://github.com/TheGetch/Debian_Themes/blob/master/Material-Black-Plum_1.8.1.zip
+unzip Material-Black-Plum_1.8.1.zip
+rm Material-Black-Plum_1.8.1.zip
+
+echo '########################### Installing MB-Plum-Suru-GLOW'
+cd /usr/share/icons/
+wget https://github.com/TheGetch/Debian_Themes/blob/master/MB-Plum-Suru-GLOW_1.8.1.zip
+unzip MB-Plum-Suru-GLOW_1.8.1.zip
+rm MB-Plum-Suru-GLOW_1.8.1.zip
+
+echo '########################### Downloading Background'
+cd /usr/share/backgrounds/
+wget https://github.com/TheGetch/Debian_Themes/blob/master/journey-wallpaper-blue.png
+
+echo '########################### Updating icon cache'
+gtk-update-icon-cache /usr/share/icons/MB-Plum-Suru-GLOW/
+
+echo '########################### Setting Theme'
+xfconf-query -c xsettings -p /Net/ThemeName -s "Material-Black-Plum"
+
+echo '########################### Setting Icons'
+xfconf-query -c xsettings -p /Net/IconThemeName -s "MB-Plum-Suru-GLOW"
+
+echo '########################### Launching Greeter Settings'
+lightdm-gtk-greeter-settings
+
+echo '########################### Finished! Dont forget to change the background! (/usr/share/backgrounds/journey-wallpaper-blue.png) '
